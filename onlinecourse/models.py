482f970a718dc31909881691e8f8f9fb055647dc
jrgomez2376 <jrgomez2376@gmail.com>
2025-04-30 20:57:18 -0500

    Update models.py
    Added Choice Class

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -114,7 +114,13 @@ class Question(models.Model):
 
 # One enrollment could have multiple submission
 # One submission could have multiple choices
+
 # One choice could belong to multiple submissions
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
