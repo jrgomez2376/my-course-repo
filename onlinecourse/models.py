b9ff6fd9f31e1d6d7ae16b9ae483a310d6869409
jrgomez2376 <jrgomez2376@gmail.com>
2025-04-30 20:38:20 -0500

    Update models.py
    Added #Question model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,7 +94,15 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+#Question model
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    grade = models.IntegerField(default=50)
 
+    def __str__(self):
+        return "Question: " + self.content
+        
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
